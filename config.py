82e5707f6d27e9dae51ef69b0015fd7265706495
BugHunter-RMA <216562981+BugHunter-RMA@users.noreply.github.com>
2026-06-11 03:18:10 +0200

    feat(config): add KIWI_BASE_URL, bump version to 2.1.0

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 # STABLE BLOCK
 # Approved. Centralized project constants.
 
-SCRIPT_VERSION = "1.0.35"
+SCRIPT_VERSION = "2.1.0"
 
 PROJECT_CODE = "TP"
 
-KIWI_URL = "https://kiwi.takeprofit.team/case/"
+KIWI_BASE_URL = "https://kiwi.takeprofit.team"
+
+KIWI_URL = f"{KIWI_BASE_URL}/case/"
